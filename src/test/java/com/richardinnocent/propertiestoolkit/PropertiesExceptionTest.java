c7cd2ebc740c6648d9653669dba984cf9c02390c
RichardInnocent <Richard.Innocent@feefo.com>
2019-02-21 16:03:32 +0000

    Added PropertiesException test

---
+++ src/test/java/com/richardinnocent/propertiestoolkit/PropertiesExceptionTest.java
@@ -0,0 +1,32 @@
+package com.richardinnocent.propertiestoolkit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author RichardInnocent
+ */
+public class PropertiesExceptionTest {
+
+  private static final String MESSAGE = "message";
+  private static final Exception CAUSE = new Exception("cause");
+
+  @Test
+  public void testMessage() {
+    assertEquals(MESSAGE, new PropertiesException(MESSAGE).getMessage());
+  }
+
+  @Test
+  public void testMessageAndCause() {
+    PropertiesException e = new PropertiesException(MESSAGE, CAUSE);
+    assertEquals(MESSAGE, e.getMessage());
+    assertEquals(CAUSE, e.getCause());
+  }
+
+  @Test
+  public void testCause() {
+    assertEquals(CAUSE, new PropertiesException(CAUSE).getCause());
+  }
+
+}
