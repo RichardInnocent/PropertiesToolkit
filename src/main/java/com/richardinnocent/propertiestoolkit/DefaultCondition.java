5adeede6e52bd4abf1a86efd7cdbd77436e88eff
RichardInnocent <Richard.Innocent@feefo.com>
2019-02-21 14:13:57 +0000

    Added Javadoc

--- src/main/java/com/richardinnocent/propertiestoolkit/DefaultCondition.java
+++ src/main/java/com/richardinnocent/propertiestoolkit/DefaultCondition.java
@@ -1,7 +1,25 @@
 package com.richardinnocent.propertiestoolkit;
 
+/**
+ * Implies when a default value/behaviour should be applied during the {@link Property#get()}
+ * method.
+ * @author RichardInnocent
+ */
 public enum DefaultCondition {
+  /**
+   * This applies when there is an error in parsing the text from the {@code Properties} value to
+   * the given type.
+   */
   PARSE_FAILS,
+  /**
+   * This applies when the text from the {@code Properties} value is {@code null} or blank (@code
+   * "").
+   */
   IS_EMPTY,
+  /**
+   * This applies when the text from the {@code Properties} file is successfully parsed, but fails
+   * any of the validation criteria.
+   * @see Property#addValidation(java.util.function.Predicate)
+   */
   IS_INVALID
 }
