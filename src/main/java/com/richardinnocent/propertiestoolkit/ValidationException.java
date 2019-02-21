78a7b8f189e4727ee7911781446de5225165682c
RichardInnocent <Richard.Innocent@feefo.com>
2019-02-21 15:53:37 +0000

    Added JavaDoc

--- src/main/java/com/richardinnocent/propertiestoolkit/ValidationException.java
+++ src/main/java/com/richardinnocent/propertiestoolkit/ValidationException.java
@@ -1,5 +1,13 @@
 package com.richardinnocent.propertiestoolkit;
 
+/**
+ * This exception is thrown when calling {@link Property#get()}, and any of the validation checks
+ * fail. This exception can be suppressed by providing the {@code Property} with some {@link
+ * DefaultSettings} that specify what value should be returned when a {@link
+ * DefaultCondition#IS_INVALID} condition is encountered.
+ * @author RichardInnocent
+ */
+@SuppressWarnings("WeakerAccess")
 public class ValidationException extends PropertiesException {
 
   private ValidationException(String message) {
@@ -10,12 +18,27 @@ public class ValidationException extends PropertiesException {
     super(message, e);
   }
 
+  /**
+   * Creates a new {@code ValidationException} with an appropriate message, given the {@code key}
+   * and {@code value}.
+   * @param key The property key.
+   * @param value The raw {@code String} value from the {@code Properties} file.
+   * @return A new {@code ValidationException} with an appropriate message.
+   */
   public static ValidationException forProperty(String key, String value) {
     return new ValidationException(getExceptionMessage(key, value));
   }
 
-  public static ValidationException forProperty(String key, String value, RuntimeException e) {
-    return new ValidationException(getExceptionMessage(key, value), e);
+  /**
+   * Creates a new {@code ValidationException} with an appropriate message. The provided {@code
+   * cause} is added as the new {@code ValidationException}'s cause.
+   * @param key The property key.
+   * @param value The raw {@code String} value from the {@code Properties} file.
+   * @param cause The exception thrown when trying to run the validation.
+   * @return A new, appropriately initialised {@code ValidationException}.
+   */
+  public static ValidationException forProperty(String key, String value, RuntimeException cause) {
+    return new ValidationException(getExceptionMessage(key, value), cause);
   }
 
   private static String getExceptionMessage(String key, String value) {
