c4710822022e42c6c6ca10a082c60e0cfa5bacb2
RichardInnocent <Richard.Innocent@feefo.com>
2019-02-21 16:03:40 +0000

    Added JavaDoc

--- src/main/java/com/richardinnocent/propertiestoolkit/PropertiesException.java
+++ src/main/java/com/richardinnocent/propertiestoolkit/PropertiesException.java
@@ -1,19 +1,40 @@
 package com.richardinnocent.propertiestoolkit;
 
+/**
+ * The parent class for all exceptions that can be thrown while calling {@link Property#get()}.
+ * @author RichardInnocent
+ */
+@SuppressWarnings("WeakerAccess")
 public class PropertiesException extends RuntimeException {
 
+  /**
+   * Creates an empty {@code PropertiesException}.
+   */
   public PropertiesException() {
     super();
   }
 
+  /**
+   * Creates a {@code PropertiesException} with the specified message.
+   * @param message The exception message.
+   */
   public PropertiesException(String message) {
     super(message);
   }
 
-  public PropertiesException(String message, Exception e) {
-    super(message, e);
+  /**
+   * Creates a {@code PropertiesException} with the specified message and cause.
+   * @param message The exception message.
+   * @param cause The cause of the exception.
+   */
+  public PropertiesException(String message, Exception cause) {
+    super(message, cause);
   }
 
+  /**
+   * Creates a {@code PropertiesException} with the specified cause.
+   * @param e The cause of the exception.
+   */
   public PropertiesException(Exception e) {
     super(e);
   }
